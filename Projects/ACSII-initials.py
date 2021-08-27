ac46e30ba1e67d4c912d68d9a1d7b042c82641ba
CSI-Andres-Juncos <23-032@sanignacio.pr>
2021-08-27 11:57:49 -0400

    Create ACSII-initials.py

---
+++ Projects/ACSII-initials.py
@@ -0,0 +1,53 @@
+print("             IIIII¯\\")
+print("            IIIIIII \\")
+print("           IIIIIIIII \\")
+print("          IIIII IIIII \\")
+print("         IIIII / IIIII \\")
+print("        IIIII /   IIIII \\")
+print("       IIIII /     IIIII \\")
+print("      IIIIIIIIIIIIIIIIIII \\")
+print("     IIIIIIIIIIIIIIIIIIIII \\")
+print("    IIIIII /        IIIIIII \\")
+print("   IIIIII /          IIIIIII \\")
+print("  IIIIII /            IIIIIII \\")
+print(" IIIIII_/              IIIIIII_\\")
+print(" ")
+print(" ")
+print("  IIIIIIIIII¯\       IIIIIIII¯|")
+print("  IIIIIIIIIII \      IIIIIIII |")
+print("  IIIIIIIIIIII \     IIIIIIII |")
+print("  IIIIIIIIIIIII \    IIIIIIII |")
+print("  IIIIIIIIIIIIII \   IIIIIIII |")
+print("  IIIIIIIIIIIIIII \  IIIIIIII |")
+print("  IIIIIIII |IIIIII \ IIIIIIII |")
+print("  IIIIIIII | IIIIII \IIIIIIII |")
+print("  IIIIIIII |  IIIIIIIIIIIIIII |")
+print("  IIIIIIII |   IIIIIIIIIIIIII |")
+print("  IIIIIIII |    IIIIIIIIIIIII |")
+print("  IIIIIIII |     IIIIIIIIIIII |")
+print("  IIIIIIII |      IIIIIIIIIII |")
+print("  IIIIIIII |       IIIIIIIIII |")
+print("  IIIIIIII_|        IIIIIIIII_|")
+print(" ")
+print(" ")
+print("""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""")
