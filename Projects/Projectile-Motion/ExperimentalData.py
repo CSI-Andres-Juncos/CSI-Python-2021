1332aedefdff81ed5c78680b744f99ad438f3b42
CSI-Andres-Juncos <23-032@sanignacio.pr>
2021-10-15 11:59:38 -0400

    Updated Part 2 of Projectile Motion

---
+++ Projects/Projectile-Motion/ExperimentalData.py
@@ -0,0 +1,9 @@
+class ExperimentalData:
+    def __init__(self,Gun:str,Caliber:str,Ammunition:str,Velocity_Ms:int,Building:str,BuildingHeight_m:int,gravity_Ms:int):
+        self.Gun=Gun
+        self.Caliber=Caliber
+        self.Ammunition=Ammunition
+        self.Velocity_Ms=Velocity_Ms
+        self.Building=Building
+        self.BuildingHeight_m=BuildingHeight_m
+        self.gravity_Ms=gravity_Ms
--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,23 +1,30 @@
 import math
+from ExperimentalData import ExperimentalData
+
 # Gun="OP-SKS"
 # Caliber="7.62x39mm"
 # Ammunition="MAI AP"
 # Velocity_Ms=875
-
 # Building="One World Trade Center"
 # BuildingHeight_m=541.3
-
 # gravity_Ms=9.81
 
 #Here im calculating the time by plugging in the formula with the variables defined on top. But first, you need to import the function of the square root.
-def ProjectileFunction(Gun:str,Caliber:str,Ammunition:str,Velocity_Ms:int,Building:str,BuildingHeight_m:int,gravity_Ms:int):
-    time_s=math.sqrt((2 * BuildingHeight_m) / gravity_Ms)
-
+def ProjectileFunction(experimentalData:ExperimentalData):
+    time_s = math.sqrt((2 * experimentalData.BuildingHeight_m) / experimentalData.gravity_Ms)
 #Now that time is calculated, the variable is plugged into another formula to calculate distance, or delta x (Δx). 
-    Distance_m=(Velocity_Ms * time_s)
+    Distance_m = (experimentalData.Velocity_Ms * time_s)
+    print(f"In this first part of the experiment, the distance of the projectile fired from a {experimentalData.Gun} was calculated. The caliber of the gun was {experimentalData.Caliber} and the projectile fired is a {experimentalData.Ammunition} with a speed of {experimentalData.Velocity_Ms}m/s. The building chosen for this experiment was the {experimentalData.Building} with a height of {experimentalData.BuildingHeight_m} meters. By using the formula for time, the time was {time_s} seconds. Then the time was plugged into another formula to calculate the distance, which was {Distance_m} meters")
 
-    print(f"In this first part of the experiment, the distance of the projectile fired from a {Gun} was calculated. The caliber of the gun was {Caliber} and the projectile fired is a {Ammunition} with a speed of {Velocity_Ms}m/s. The building chosen for this experiment was the {Building} with a height of {BuildingHeight_m} meters. By using the formula for time, the time was {time_s} seconds. Then the time was plugged into another formula to calculate the distance, which was {Distance_m} meters")
+# experimentalData ={
+#     "Gun=" : "OP-SKS",
+#     "Caliber" : "7.62x39mm",
+#     "Ammunition" : "MAI AP",
+#     "Velocity_Ms" : 875,
+#     "Building=" : "One World Trade Center",
+#     "BuildingHeight_m" : 541.3,
+#     "gravity_Ms" : 9.81
+# }
 
-
-ProjectileFunction("OP-SKS","7.62x39mm","MAI AP",875,"One World Trade Center",541.3,9.81)
-
+experimentalData = ExperimentalData("OP-SKS","7.62x39mm","MAI AP",875,"One World Trade Center",541.3,9.81)
+ProjectileFunction(experimentalData)
