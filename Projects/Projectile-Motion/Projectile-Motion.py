af3174d46ec53a1207e85d4f0caec16f06a9d4e1
CSI-Andres-Juncos <23-032@sanignacio.pr>
2021-10-15 11:31:37 -0400

    Update Projectile-Motion.py

--- Projects/Projectile-Motion/Projectile-Motion.py
+++ Projects/Projectile-Motion/Projectile-Motion.py
@@ -1,18 +1,23 @@
-Gun="OP-SKS"
-Caliber="7.62x39mm"
-Ammunition="MAI AP"
-Velocity_Ms=875
+import math
+# Gun="OP-SKS"
+# Caliber="7.62x39mm"
+# Ammunition="MAI AP"
+# Velocity_Ms=875
 
-Building="One World Trade Center"
-BuildingHeight_m=541.3
+# Building="One World Trade Center"
+# BuildingHeight_m=541.3
 
-gravity_Ms=9.81
+# gravity_Ms=9.81
 
 #Here im calculating the time by plugging in the formula with the variables defined on top. But first, you need to import the function of the square root.
-import math
-time_s=math.sqrt((2 * BuildingHeight_m) / gravity_Ms)
+def ProjectileFunction(Gun:str,Caliber:str,Ammunition:str,Velocity_Ms:int,Building:str,BuildingHeight_m:int,gravity_Ms:int):
+    time_s=math.sqrt((2 * BuildingHeight_m) / gravity_Ms)
 
 #Now that time is calculated, the variable is plugged into another formula to calculate distance, or delta x (Δx). 
-Distance_m=(Velocity_Ms * time_s)
+    Distance_m=(Velocity_Ms * time_s)
 
-print(f"In this first part of the experiment, the distance of the projectile fired from a {Gun} was calculated. The caliber of the gun was {Caliber} and the projectile fired is a {Ammunition} with a speed of {Velocity_Ms}m/s. The building chosen for this experiment was the {Building} with a height of {BuildingHeight_m} meters. By using the formula for time, the time was {time_s} seconds. Then the time was plugged into another formula to calculate the distance, which was {Distance_m} meters")
+    print(f"In this first part of the experiment, the distance of the projectile fired from a {Gun} was calculated. The caliber of the gun was {Caliber} and the projectile fired is a {Ammunition} with a speed of {Velocity_Ms}m/s. The building chosen for this experiment was the {Building} with a height of {BuildingHeight_m} meters. By using the formula for time, the time was {time_s} seconds. Then the time was plugged into another formula to calculate the distance, which was {Distance_m} meters")
+
+
+ProjectileFunction("OP-SKS","7.62x39mm","MAI AP",875,"One World Trade Center",541.3,9.81)
+
