357be88e45947845ed26fbbe8c1cd9996c7606c3
CSI-Andres-Juncos <23-032@sanignacio.pr>
2022-02-14 12:03:32 -0400

    Update Hangman.py

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,7 +1,11 @@
+from contextlib import nullcontext
 from http.client import responses
 import json, ssl
+from operator import contains
+import numbers
 import os
 from pathlib import Path
+from tracemalloc import start
 import urllib.request
 from Coffee import Coffee
 
@@ -99,3 +103,26 @@ print(steps[0])
 print(coffee.blend_name)
 
 print(len(coffee.blend_name)*" _")
+
+
+def input_function():
+    while(True):
+        letter = input("Imput letter")
+
+        if(len(letter) != 1):
+            print("Not a letter")
+            continue
+        if (letter.isdigit()):
+            print("error")
+            continue
+        return letter
+
+
+
+
+    
+
+    
+
+    
+
